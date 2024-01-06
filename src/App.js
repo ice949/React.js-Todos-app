b519c64ac1d0fc412f9178c8154db7999c67797f
Justine Imasiku <justineimasiku21@gmail.com>
2024-01-06 14:56:06 +0000

    create the todos page and complete its UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Signup from './Pages/Signup/Signup';
 import Login from './Pages/Login/Login';
+import Todos from './Pages/Todos/Todos';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup/>} />
         <Route path="login" element={<Login />} />
-        <Route path="todos" element={<h1>Not Found</h1>} />
+        <Route path="todos" element={<Todos />} />
       </Routes>
     </Router>
   );
